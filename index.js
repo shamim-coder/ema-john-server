127622bbdcc53b81a43a4b229749adc0d3f2e9a0
Shamim Reza <wpshamim@gmail.com>
2022-06-24 01:44:43 +0600

    update

--- index.js
+++ index.js
@@ -68,9 +68,9 @@ const run = async () => {
 
         // get orders
         app.get("/orders", async (req, res) => {
-            const email = req.query?.email;
+            const email = req.query.email;
             console.log(email);
-            const query = { email };
+            const query = {};
             const cursor = orderCollections.find(query);
             const orders = await cursor.toArray();
             res.send(orders);
